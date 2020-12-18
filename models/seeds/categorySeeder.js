cf2bd6fce3b988a49a264a3beff282a60ef51e6b
linchuaccount <younchu123@gmail.com>
2020-12-18 18:49:14 +0800

    refactor: modify mongoose config

--- models/seeds/categorySeeder.js
+++ models/seeds/categorySeeder.js
@@ -1,15 +1,9 @@
 const Record = require('../record')
-const mongoose = require('mongoose')
 const categoryData = require('./categorys.json')
 
-mongoose.connect('mongodb://localhost/expense-tracker', { useNewUrlParser: true, useUnifiedTopology: true })
+const db = require('../../config/mongoose')
 
-const db = mongoose.connection
-db.on('error', () => {
-  console.log('mongodb error!')
-})
 db.once('open', () => {
-  console.log('mongodb connected!')
   categoryData.forEach(item => {
     Record.create(item)
   })
--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -1,21 +1,12 @@
 const Record = require('../record')
-const mongoose = require('mongoose')
 const recordData = require('./records.json')
 
-mongoose.connect('mongodb://localhost/expense-tracker', { useNewUrlParser: true, useUnifiedTopology: true })
+const db = require('../../config/mongoose')
 
-const db = mongoose.connection
-db.on('error', () => {
-  console.log('mongodb error!')
-})
 db.once('open', () => {
-  console.log('mongodb connected!')
   //參考同學作業 forEach
-  recordData.forEach( item => {
+  recordData.forEach(item => {
     Record.create(item)
   })
   console.log('done!')
-  
-  //關閉db
-  .then(() => db.close())
 })
