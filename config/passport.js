337e895db5dd6d544f35ea15c709f0493339d61c
linchuaccount <younchu123@gmail.com>
2021-02-16 14:04:24 +0800

    feat: add facebook login & dotenv

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,6 @@
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
+const FacebookStrategy = require('passport-facebook').Strategy
 const User = require('../models/user')
 const bcrypt = require('bcryptjs')
 
@@ -7,6 +8,7 @@ module.exports = app => {
   //初始化模組
   app.use(passport.initialize())
   app.use(passport.session())
+
   //設定本地登入策略
   passport.use(new LocalStrategy({ usernameField: 'email', passReqToCallback: true }, (req, email, password, done) => {
     User.findOne({ email })
@@ -23,6 +25,29 @@ module.exports = app => {
       })
       .catch(err => done(err, false))
   }))
+  //設定FB登入策略
+  passport.use(new FacebookStrategy({
+    clientID: process.env.FACEBOOK_ID,
+    clientSecret: process.env.FACEBOOK_SECRET,
+    callbackURL: process.env.FACEBOOK_CALLBACK,
+    profileFields: ['email', 'displayName']
+  }, (accessToken, refreshToken, profile, done) => {
+    const { name, email } = profile._json
+    User.findOne({ email })
+      .then(user => {
+        if (user) return done(null, user)
+        const randomPassword = Math.random().toString(36).slice(-8)
+        bcrypt
+          .genSalt(10)
+          .then(salt => bcrypt.hash(randomPassword, salt))
+          .then(hash => User.create({
+            name, email, password: hash
+          }))
+          .then(user => done(null, user))
+          .catch(err => done(err, false))
+      })
+  }
+  ))
 
   //序列化和反序列化
   passport.serializeUser((user, done) => {
